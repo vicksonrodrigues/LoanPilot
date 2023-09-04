fc1db564b4a2e6d29040204daaa86e5119773cab
Vickson Rodrigues <55660155+vicksonrodrigues@users.noreply.github.com>
2023-09-04 18:20:06 +0530

    changed frontend ui framework

---
+++ client/.eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["airbnb", "prettier"],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["prettier"],
+  rules: {
+    "prettier/prettier": "error",
+    "import/no-unresolved": "off",
+    "react/jsx-filename-extension": "off",
+    "max-len": [1, 200],
+    "no-shadow": 0,
+    "no-plusplus": 0,
+    "react/prop-types": [
+      1,
+      {
+        skipUndeclared: true,
+      },
+    ],
+    "react/function-component-definition": [
+      "error",
+      {
+        namedComponents: ["function-declaration", "arrow-function"],
+        unnamedComponents: "arrow-function",
+      },
+    ],
+  },
+};
---
+++ client/src/App.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const App = () => <div className="App"> Loan Pilot</div>;
+
+export default App;
